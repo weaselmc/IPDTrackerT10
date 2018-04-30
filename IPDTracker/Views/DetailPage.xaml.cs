32b345e2dacec8f84d9ba9fd5a4a57b5dbdf06d6
Mark Buttsworth <buttsm@tdm.local>
2018-04-30 13:24:17 +0800

    Add project files.

---
+++ IPDTracker/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using IPDTracker.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace IPDTracker.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
---
+++ IPDTracker/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using IPDTracker.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace IPDTracker.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
